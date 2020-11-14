5aae0f110eeff01773d1d0f330a44753186fd3ab
NeerajRegaltos <70850450+NeerajRegaltos@users.noreply.github.com>
2020-11-14 19:13:24 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const translate = require('translate-google');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const path = require("path");
+const { code } = require("./language");
+const env = require("dotenv");
+const app = express();
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static('public'))
+
+var keyArray = Object.keys(code);
+
+app.get("/", (req, res) => {
+    res.render("home", { options: keyArray });
+});
+
+
+app.post("/", async (req, res) => {
+    const { text, lang } = req.body;
+    for (var i = 0; i < keyArray.length; i++) {
+        if (keyArray[i] === lang) {
+            var x = code[lang];
+        }
+    }
+
+    const result = await translate(text, { to: x })
+    res.render("ans", { sentence: result });
+});
+
+app.post("/info", (req, res) => {
+    res.render("info")
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`started on server ${port}`);
+});
