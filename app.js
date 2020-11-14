63685500faef8025f7b007c970cbbaefe3f73c14
NeerajRegaltos <70850450+NeerajRegaltos@users.noreply.github.com>
2020-11-14 21:10:31 +0530

    Changed little code

--- app.js
+++ app.js
@@ -25,11 +25,12 @@ app.post("/", async (req, res) => {
     for (var i = 0; i < keyArray.length; i++) {
         if (keyArray[i] === lang) {
             var x = code[lang];
+            const result = await translate(text, { to: x })
+            res.render("ans", { sentence: result });
         }
     }
 
-    const result = await translate(text, { to: x })
-    res.render("ans", { sentence: result });
+
 });
 
 app.post("/info", (req, res) => {
